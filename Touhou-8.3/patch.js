fffa9daffdc3847e6e49ad7f44f9584eeb0918e9
Nutzer <reztun@outlook.de>
2014-08-24 01:23:24 +0200

    TH8.3 0.01a
    Update 1

---
+++ Touhou-8.3/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+	],
+	"id": "Touhou-8.3",
+	"servers": [
+		"https://raw.githubusercontent.com/Nutzer/Toouhou-8.3/master/Touhou-8.3/"
+	],
+	"title": "TNutzer's Touhou 8.3"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "reztun@outlook.de",
+	"id": "Touhou 8.3",
+	"patches": {
+		"Touhou-8.3": "TNutzer's Touhou 8.3"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/Nutzer/Toouhou-8.3/master"
+	],
+	"title": "Nutzer's Touhou-8.3"
+}
