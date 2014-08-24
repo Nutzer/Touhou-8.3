99b3189515ed95baa5b8b2d04812a83c6206ad7e
Nutzer <reztun@outlook.de>
2014-08-24 06:39:28 +0200

    e

--- Touhou-8.3/patch.js
+++ Touhou-8.3/patch.js
@@ -1,5 +1,8 @@
 {
-	"dependencies": [],
+    "dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin"
+    ],
 	"id": "Touhou-8.3",
 	"servers": [
 		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/Touhou-8.3/Touhou-8.3/",
