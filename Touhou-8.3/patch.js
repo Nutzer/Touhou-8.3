0976f1ac4369e089b1a3410fbffaadf8c2cae9b7
Nutzer <reztun@outlook.de>
2014-09-01 22:37:54 +0200

    Post-Finally IV

--- Touhou-8.3/patch.js
+++ Touhou-8.3/patch.js
@@ -5,7 +5,8 @@
 	],
 	"id": "Touhou-8.3",
 	"servers": [
-		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/Touhou-8.3/Touhou-8.3/"
+		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/Touhou-8.3/Touhou-8.3/",
+		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/Touhou-8.3/"
 	],
 	"title": "Nutzer's Touhou 8.3"
 }
