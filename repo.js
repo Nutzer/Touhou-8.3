e1bb45c017eb5c9c2ba1933060851041294423da
Nutzer <reztun@outlook.de>
2014-09-01 22:33:14 +0200

    Post-Finally III

--- repo.js
+++ repo.js
@@ -5,8 +5,7 @@
 		"Touhou-8.3": "Nutzer's Touhou 8.3"
 	},
 	"servers": [
-		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/Touhou-8.3/",
-		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/"
+		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/Touhou-8.3/"
 	],
 	"title": "Nutzer"
 }
