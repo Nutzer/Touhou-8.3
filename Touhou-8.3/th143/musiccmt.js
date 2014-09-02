61fda3af6fbc54835127df62ca1b95a64c0d4b76
Nutzer <reztun@outlook.de>
2014-09-02 15:16:22 +0200

    Post-Finally VIII

---
+++ Touhou-8.3/th143/musiccmt.js
@@ -0,0 +1,71 @@
+{
+    "1": [
+        "@",
+        "　",
+        "♪Led On by a Cow to Visit Zenkou Temple",
+        "　                                                by Zun",
+        "        Title Theme",
+        "Author's Comment:",
+        "　キャラと曲が結びつかないゲームなので、",
+        "　一般的な戦闘曲みたいなのになりました。",
+        "　ステージの長さと曲の長さが釣り合い取れないのは",
+        "　何度もリトライすることが前提だからです。"
+    ],
+    "2": [
+        "@",
+        "　",
+        "Earliest stages theme.",
+        "",
+        "Because the music in this game is not related to the characters,",
+        "it feels more like generic combat music.",
+        "I couldn't make track lengths fit stage lengths,",
+        "because they're meant to be endlessly retried."
+    ],
+    "3": [
+        "@",
+        "　",
+        "Early stages theme.",
+        "",
+        "I gave this track a night-like feel since the stages take place at night.",
+        "It has a rushing feel, so try not to linger in these stages for too long."
+    ],
+    "4": [
+        "@",
+        "　",
+        "Middle stages theme.",
+        "",
+        "This one is completely different and cheerful.",
+        "By this point, you can't beat the stages",
+        "quite so easily, can you?"
+    ],
+    "5": [
+        "@",
+        "　",
+        "Last stages theme.",
+        "",
+        "Went with a moderate theme for the end.",
+        "But this danmaku is hell. Maybe this song invokes desperation instead.",
+        "",
+        "Perhaps these cards really are impossible?"
+    ],
+    "6": [
+        "@",
+        "　",
+        "Previously used in <i$Double Dealing Character>."
+    ],
+    "7": [
+        "@",
+        "　",
+        "Previously used in <i$Double Dealing Character>."
+    ],
+    "8": [
+        "@",
+        "　",
+        "Previously used in <i$Double Dealing Character>."
+    ],
+    "9": [
+        "@",
+        "　",
+        "Previously used in <i$Mountain of Faith>."
+    ]
+}
