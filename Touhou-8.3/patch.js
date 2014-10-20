593d8f290a7f357cbda444e26c2631ae52b52e7e
Nutzer <reztun@outlook.de>
2014-10-21 01:56:57 +0200

    Super-Hyper-Mega-Finally IV

--- Touhou-8.3/patch.js
+++ Touhou-8.3/patch.js
@@ -5,7 +5,8 @@
 	],
 	"id": "Touhou-8.3",
 	"servers": [
-		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/Touhou-8.3/"
+		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/Touhou-8.3/",
+		"http://thcrap.nmlgc.net/repos/nutzer/Touhou-8.3/"
 	],
 	"title": "Nutzer's Touhou 8.3 (Demo 0.1a)"
 }
