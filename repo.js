eb8207a76355191a520962938968ca3a241c07f7
Nutzer <reztun@outlook.de>
2014-10-21 01:52:54 +0200

    Super-Hyper-Mega-Finally III

--- repo.js
+++ repo.js
@@ -5,7 +5,8 @@
 		"Touhou-8.3": "Nutzer's Touhou 8.3 (Demo 0.1a)"
 	},
 	"servers": [
-		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/"
+		"https://raw.githubusercontent.com/Nutzer/Touhou-8.3/master/", 
+        "http://thcrap.nmlgc.net/repos/nutzer/"
 	],
 	"title": "Nutzer"
 }
